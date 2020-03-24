d85104ef56e56a4792a200f898a257df4cc3ae8a
YukiKuroda <kuroda@leapp.jp>
2020-03-24 21:06:29 +0900

    2020年3月24日

--- includes/wp-annual-event-admin-db.php
+++ includes/wp-annual-event-admin-db.php
@@ -4,6 +4,23 @@ class Annual_Event_Admin_Db {
 
   public function __construct() {
     global $wpdb;
-    $this->table_name = $wpdb->prefix . 'annual-event';
+    $this->table_name = $wpdb->prefix . 'annual_event';
+
+    $this->create_table();
+  }
+
+private function create_table() {
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+  $prepare = $wpdb->prepare( "SHOW TABLES LIKE %s", $this->table_name );
+  $is_db_exists = $wpdb->get_var( $prepare );
+  var_dump( $is_db_exists );
+
+  $query  = "";
+  $query .= "CREATE TABLE " .$this->table_name . "(";
+  $query .= "id MEDIUMINT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY";
+  $query .=");";
+
+  // dbDelta( $query );
   }
 }
